0309cf032fd8b15bbed603b4176c7ec9346ce558
rjmacarthy <richardmacarthy@hotmail.com>
2016-08-31 11:37:28 +0100

    List json files

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var fs = require('fs');
 var uuid = require('node-uuid');
 var path = require('path');
+var glob = require('glob');
 var router = express.Router();
 
 /* GET home page. */
@@ -17,6 +18,13 @@ router.post('/api/webhook', function (req, res, next) {
       res.status(200).json({ success: true });
     }
   });
+});
+
+
+router.post('/list', function (req, res, next) {
+  glob(path.join(__dirname, '../public/**/*.json'), null, function (er, files) {
+    res.status(200).json(files);
+  })
 });
 
 
